1bd1692a540ce7cdddb85c2423c130f95fee94db
Abdul Rahim <raheemabdur106@gmail.com>
2026-03-24 10:48:34 +0500

    models completed

---
+++ models/User/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false                   // Ensures password isn't returned in simple queries / API response
+  },
+  role: {
+    type: String,
+    default: 'user' 
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true 
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
