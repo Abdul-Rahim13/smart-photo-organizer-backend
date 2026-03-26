de1e5f96dc11cb59ba0ae6c7e2b78aa50ea524b0
Abdul Rahim <raheemabdur106@gmail.com>
2026-03-26 18:10:41 +0500

    added Photo Upload Route

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const cors = require('cors')
 const morgan = require('morgan')
 const PORT = process.env.PORT || 3000
 const connectDB = require('./config/db')
+const authRoute = require('./routes/authRoute/auth')
+const photoRoute = require('./routes/photoRoute/photo')
+
 
 connectDB()
+app.use('/uploads', express.static('uploads'))
 app.use(morgan('dev'))                  // middleware for Express that logs every HTTP request coming to your server
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -14,6 +18,12 @@ app.use(cors({
     origin: 'http://localhost:3000',         // frontend URL
   credentials: true,                        // allow cookies if needed
 }))
+
+// Auth Routes
+app.use('/api/auth', authRoute)
+// Photo Upload Routes
+app.use('/api', photoRoute)
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
