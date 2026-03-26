44881c7642a2874dea5ab11253f3c22b12f95f7e
Abdul Rahim <raheemabdur106@gmail.com>
2026-03-26 10:03:23 +0500

    added mongoDb Connection

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 const PORT = process.env.PORT || 3000
+const connectDB = require('./config/db')
 
-
+connectDB()
 app.use(morgan('dev'))                  // middleware for Express that logs every HTTP request coming to your server
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -12,3 +14,7 @@ app.use(cors({
     origin: 'http://localhost:3000',         // frontend URL
   credentials: true,                        // allow cookies if needed
 }))
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
