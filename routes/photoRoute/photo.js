379f5fe65b3152bdd1085876dc6ded0d82b8e721
Abdul Rahim <raheemabdur106@gmail.com>
2026-03-29 00:59:35 +0500

    added getallphotos, filterPhotos, getByscence routes

--- routes/photoRoute/photo.js
+++ routes/photoRoute/photo.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const router = express.Router()
-
 const upload = require('../../middleware/uploadMiddleware')
 const { authMiddleware } = require('../../middleware/AuthMiddleware')
-const { uploadPhoto } = require('../../controller/photoController/photo')
+const { uploadPhoto, getAllPhotos, filterPhotos, getByScene, getTopPhotos, getPhotosWithFaces, getFlaggedPhotos, searchPhotos } = require('../../controller/photoController/photo')
 
-router.post(
-    '/upload',
-    authMiddleware,
-    upload.array('images', 5),
-    uploadPhoto
-)
+
+
+router.post('/upload', authMiddleware, upload.array('images', 5), uploadPhoto )
+
+router.get('/', authMiddleware, getAllPhotos)
+
+router.get('/filter', authMiddleware, filterPhotos)
+
+router.get('/scene/:type', authMiddleware, getByScene)
+
+router.get('/top', authMiddleware, getTopPhotos)
+
+router.get('/faces', authMiddleware, getPhotosWithFaces)
+
+router.get('/flagged', authMiddleware, getFlaggedPhotos)
+
+router.get('/search', authMiddleware, searchPhotos)
+
 
 module.exports = router
