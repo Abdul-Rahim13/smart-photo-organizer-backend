7311c1135f1ad49dcee05aeefdb59d88d3d7f055
Abdul Rahim <raheemabdur106@gmail.com>
2026-05-08 00:41:44 +0500

    install depndencies for nodemailer, add utils + sendMail logic and forgot password in controller

--- routes/authRoute/auth.js
+++ routes/authRoute/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {register, login} = require ('../../controller/authController/auth')
+const {register, login, forgotPassword } = require ('../../controller/authController/auth')
 
 
 router.post('/register', register)
 router.post('/login', login)
+router.post('/forget-password', forgotPassword )
 
 module.exports = router
