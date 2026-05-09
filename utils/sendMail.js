8bff2a39f8e737514db5670604751c721caa0730
Abdul Rahim <raheemabdur106@gmail.com>
2026-05-10 00:58:31 +0500

    debug resend

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,12 +3,17 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (to, subject, text) => {
-    await resend.emails.send({
-        from: "onboarding@resend.dev",
+
+    console.log("RESEND KEY:", process.env.RESEND_API_KEY ? "EXISTS" : "MISSING");
+
+    const result = await resend.emails.send({
+        from: "SmartEditor AI <onboarding@resend.dev>",
         to,
         subject,
         text,
     });
+
+    console.log("RESEND RESULT:", JSON.stringify(result));
 };
 
 module.exports = sendMail;
